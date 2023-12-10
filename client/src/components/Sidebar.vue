75cc39a36dd39d011dbc82f1a33c1e1e71810cf6
rizzacarla <rizzacarla.12@gmail.com>
2023-12-09 21:14:54 -0800

    Change sidenav

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -3,23 +3,21 @@
     <v-list>
       <v-list-item prepend-avatar="https://storage.cloud.google.com/rizza-marzo-portfolio-bucket/assets/resume%20photo%20circle-cropped.png" title="Rizza Carla Modina Marzo" nav>
         <template v-slot:append>
-            <v-icon variant="text" icon="chevron_right" @click.stop="rail = !rail"></v-icon>
+            <v-icon variant="text" icon="chevron_right" @click.stop="closeSidebar"></v-icon>
         </template>
       </v-list-item>
-      <v-list-item>
+      <v-list-item :class="{ 'center-download .': rail === false }">
         <template v-slot:prepend v-if="rail === true">
           <v-icon icon="download"></v-icon>
         </template>
-        <template v-slot:append>
-          <v-btn rounded="xs" :class="{ 'center-download': rail === false }">Download Resume</v-btn>
-        </template>
+        <v-btn rounded="xs">Download Resume</v-btn>
       </v-list-item>
     </v-list>
       
     <v-divider></v-divider>
     
     <v-list>
-      <v-list-group value="Personal Info">
+      <v-list-group>
           <template v-slot:activator="{ props }">
             <v-list-item
               v-bind="props"
@@ -27,14 +25,13 @@
               title="Personal Info"
             ></v-list-item>
           </template>
-          <v-list-item prepend-icon="home" value="Name"><span>Rizza Carla Modina Marzo</span></v-list-item>
-          <v-list-item prepend-icon="home" value="Number"><span>(909) 904-9029</span></v-list-item>
-          <v-list-item prepend-icon="home" value="Email"><span>rizzacarla.12@gmail.com</span></v-list-item>
+          <v-list-item prepend-icon="phone" value="Number"><span>(909) 904-9029</span></v-list-item>
+          <v-list-item prepend-icon="email" value="Email"><span>rizzacarla.12@gmail.com</span></v-list-item>
           <v-list-item prepend-icon="home" value="Address"><span>Rancho Cucamonga, CA 91739</span></v-list-item>
-          <v-list-item prepend-icon="home" value="LinkedIn"><span>LinkedIn</span></v-list-item>
-          <v-list-item prepend-icon="home" value="Github"><span>Github</span></v-list-item>
+          <v-list-item prepend-icon="chat" value="LinkedIn"><span>LinkedIn</span></v-list-item>
+          <v-list-item prepend-icon="laptop" value="Github"><span>Github</span></v-list-item>
       </v-list-group>
-      <v-list-group value="Skills">
+      <v-list-group>
           <template v-slot:activator="{ props }">
             <v-list-item
               v-bind="props"
@@ -55,10 +52,14 @@ export default {
   name: "Sidebar",
   data() {
     return {
-      drawer: true,
       rail: true,
     }
   },
+  methods: {
+    closeSidebar() {
+      this.rail = !this.rail;
+    },
+  }
 }
 </script>
 
@@ -69,7 +70,8 @@ export default {
 }
 .center-download {
   position: relative;
-  right: 15%;
+  display: flex;
+  justify-content: center;
 }
 :deep(.v-avatar) {
   position: relative;
